5d938a8e89f14398c215acf849e1106238d78349
issleepyhead <jomari.tenorio03@gmail.com>
2024-01-20 23:10:03 +0800

    added loop

---
+++ conditional_statements/loops.rb
@@ -0,0 +1,16 @@
+# TITLE: loops
+# DATE CREATED: 2024-01-20 23:05.47
+# AUTHOR: issleepyhead
+
+
+# while loop in ruby
+age = 18
+while age > 1
+  age -= 1
+end
+
+
+# until loop will run as long as the expression evaluates to false
+until age < 18
+  age -= 1
+end 
