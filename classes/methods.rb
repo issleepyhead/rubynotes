8e027c0ec73c754991d8dab2971b41d75ef4951f
issleepyhead <jomari.tenorio03@gmail.com>
2024-01-21 23:21:30 +0800

    added methods

---
+++ classes/methods.rb
@@ -0,0 +1,13 @@
+# TITLE: methods
+# DATE CREATED: 2024-01-21 23:18.26
+# AUTHOR: issleepyhead
+
+# defining a method
+def one_plus_one
+  1 + 1
+end
+
+# short syntax for method
+def one_plus_two = 1 + 2
+
+
